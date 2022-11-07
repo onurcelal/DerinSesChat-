8756ed8618d06dabd8efcc2a60e5f7a6826e4977
onurcelal <53437549+onurcelal@users.noreply.github.com>
2022-11-07 22:10:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     arg_dict = vars(args)
     print_args(args, parser)
     
-    # Hide GPUs from Pytorch to force CPU processing
+   
     if arg_dict.pop("cpu"):
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
@@ -80,67 +80,35 @@ if __name__ == '__main__':
 
     ## Teste başlıyor
     print("Yapılandırmanız birincil girdi ile test ediliyor.")
-    # Forward an audio waveform of zeroes that lasts 1 second. Notice how we can get the encoder's
-    # sampling rate, which may differ.
-    # If you're unfamiliar with digital audio, know that it is encoded as an array of floats
-    # (or sometimes integers, but mostly floats in this projects) ranging from -1 to 1.
-    # The sampling rate is the number of values (samples) recorded per second, it is set to
-    # 16000 for the encoder. Creating an array of length <sampling_rate> will always correspond
-    # to an audio of 1 second.
+   
+   
     print("\Kodlayıcı test ediyor...")
     encoder.embed_utterance(np.zeros(encoder.sampling_rate))
 
-    # Create a dummy embedding. You would normally use the embedding that encoder.embed_utterance
-    # returns, but here we're going to make one ourselves just for the sake of showing that it's
-    # possible.
+    
     embed = np.random.rand(speaker_embedding_size)
-    # Embeddings are L2-normalized (this isn't important here, but if you want to make your own
-    # embeddings it will be).
     embed /= np.linalg.norm(embed)
-    # The synthesizer can handle multiple inputs with batching. Let's create another embedding to
-    # illustrate that
     embeds = [embed, np.zeros(speaker_embedding_size)]
     texts = ["test 1", "test 2"]
     print("\Sentezci test ediyor... (loading the model will output a lot of text)")
     mels = synthesizer.synthesize_spectrograms(texts, embeds)
     
-    # The vocoder synthesizes one waveform at a time, but it's more efficient for long ones. We
-    # can concatenate the mel spectrograms to a single one.
+   
     mel = np.concatenate(mels, axis=1)
-    # The vocoder can take a callback function to display the generation. More on that later. For
-    # now we'll simply hide it like this:
     no_action = lambda *args: None
     print("\Ses kodlayıcı test ediyor...")
-    # For the sake of making this test short, we'll pass a short target length. The target length
-    # is the length of the wav segments that are processed in parallel. E.g. for audio sampled
-    # at 16000 Hertz, a target length of 8000 means that the target audio will be cut in chunks of
-    # 0.5 seconds which will all be generated together. The parameters here are absurdly short, and
-    # that has a detrimental effect on the quality of the audio. The default parameters are
-    # recommended in general.
     vocoder.infer_waveform(mel, target=200, overlap=50, progress_callback=no_action)
     num_generated = ""    
     girdi_yolu = Path("/content/drive/MyDrive/DerinSesChat/steve.wav")
     ## Hafızaya İşleniyor
-    # First, we load the wav using the function that the speaker encoder provides. This is
-    # important: there is preprocessing that must be applied.
-
-    # The following two methods are equivalent:
-    # - Directly load from the filepath:
     preprocessed_wav = encoder.preprocess_wav(girdi_yolu)
-    # - If the wav is already loaded:
     original_wav, sampling_rate = librosa.load(str(girdi_yolu))
     preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
     print("HAFIZAYA BAŞARIYLA ALINDI")
 
-    # Then we derive the embedding. There are many functions and parameters that the
-    # speaker encoder interfaces. These are mostly for in-depth research. You will typically
-    # only use this function (with its default parameters):
+  
     embed = encoder.embed_utterance(preprocessed_wav)
     print("KLONLANIYOR")
-
-    
-    
-    
     for bşlk in veri["intents"]:         
       yanıt = bşlk["response"]  
   
@@ -148,17 +116,12 @@ if __name__ == '__main__':
     
     
     ## Spectogram oluşturuluyor
-      
-    # If seed is specified, reset torch seed and force synthesizer reload
     if args.seed is not None:
         torch.manual_seed(args.seed)
         synthesizer = Synthesizer(args.syn_model_fpath)
   
-    # The synthesizer works in batch, so you need to put your data in a list or numpy array
     texts = [text]
     embeds = [embed]
-    # If you know what the attention layer alignments are, you can retrieve them here by
-    # passing return_alignments=True
     specs = synthesizer.synthesize_spectrograms(texts, embeds)
     spec = specs[0]
     print("Spectrogram melodisi oluşturuluyor")
@@ -167,22 +130,14 @@ if __name__ == '__main__':
     ## Dalga formu oluşturuluyor
     print("Dalga formu sentezleniyor:")
 
-    # If seed is specified, reset torch seed and reload vocoder
+    
     if args.seed is not None:
         torch.manual_seed(args.seed)
         vocoder.load_model(args.voc_model_fpath)
-
-    # Synthesizing the waveform is fairly straightforward. Remember that the longer the
-    # spectrogram, the more time-efficient the vocoder.
+        
     generated_wav = vocoder.infer_waveform(spec)
-
-
     ## Sonraki jenerasyon
-    # There's a bug with sounddevice that makes the audio cut one second earlier, so we
-    # pad it.
     generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
-
-    # Trim excess silences to compensate for gaps in spectrograms (issue #53)
     generated_wav = encoder.preprocess_wav(generated_wav)
     dosya_adı = "demo_çıktı.wav" 
     sf.write(dosya_adı, generated_wav.astype(np.float32), synthesizer.sample_rate)
